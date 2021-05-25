2b653ad899681df96b1827d9c4d8bb109d696fbf
dDanthe <diegoandrestovar@gmail.com>
2021-05-25 17:37:19 -0500

    docs folder

---
+++ docs/precache-manifest.192de2bf228bbc135af18e805b3bfc14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "493972a0d50b51dd015c2cdc55ea71b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "36f0b7a4979e4d2307ef",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36f0b7a4979e4d2307ef",
+    "url": "/static/js/main.573242cd.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
